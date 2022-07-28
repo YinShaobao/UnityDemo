841f81c5471fbb23263191b10ea86a007da00161
Yin <jieyin101@gmail.com>
2022-07-28 17:14:36 +0800

    UGUI DEMO

---
+++ UGUI_Image/Assets/Corner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Canvas mainCanvas;
+    public float minX;
+    public float maxY;
+
+    void Start()
+    {
+        mainCanvas = GetComponent<Canvas>();
+        minX = mainCanvas.GetComponent<RectTransform>().position.x + mainCanvas.GetComponent<RectTransform>().rect.xMin;
+        maxY = mainCanvas.GetComponent<RectTransform>().position.y + mainCanvas.GetComponent<RectTransform>().rect.yMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UGUI_Image/Assets/Movement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool moveLeft;
+    private bool moveRight;
+    private float horizontalMove;
+    public Corner pos;
+
+    public float speed = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //pos = canvas.GetComponent<Corner>();
+        moveLeft = false;
+        moveRight = false;
+    }
+
+    public void DownLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void UpLeft()
+    {
+        moveLeft = false;
+    }
+
+    public void DownRight()
+    {
+        moveRight = true;   
+    }
+
+    public void UpRight()
+    {
+        moveRight = false;
+    }
+
+    public void TopLeft()
+    {
+        transform.position = new Vector2(0.0f, Screen.height);
+    }
+
+    private void MovementPlayer()
+    {
+        if (moveLeft)
+        {
+            horizontalMove = -speed * Time.deltaTime;
+        }
+        else if (moveRight)
+        {
+            horizontalMove = speed * Time.deltaTime;
+        }
+        else
+        {
+            horizontalMove = 0; 
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MovementPlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMove, 0.0f);
+    }
+}
